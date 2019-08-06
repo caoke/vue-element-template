34ed2138141272dcdb16073e81b7c279309e0f05
kecao <coco.cao@sf-express.com>
2019-08-06 18:45:36 +0800

    canvas

--- src/views/canvas/index.vue
+++ src/views/canvas/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app-container">
-    <canvas id="myCanvas" />
+    <img id="icon" src="../../assets/jizhan.png" alt="" @click="add()">
+    <canvas ref="myCanvas" class="canvas" @click="addIcon" />
   </div>
 </template>
 
@@ -9,20 +10,47 @@ export default {
   data() {
     return {
       c: null,
-      ctx: null
+      ctx: null,
+      isAdd: false
     }
   },
   mounted() {
     this.init()
+    console.log(`{$sideBarWidth}`)
   },
   methods: {
     init() {
-      this.c = document.getElementById('myCanvas')
+      this.c = this.$refs.myCanvas
       if (this.c.getContext) {
         this.ctx = this.c.getContext('2d')
       }
-      this.drawLine()
-      this.drawCircle()
+    },
+    add() {
+      this.isAdd = true
+    },
+    addIcon(event) {
+      const img = document.getElementById('icon')
+      var mouse = {
+        x: event.clientX + document.documentElement.scrollLeft + document.body.scrollLeft - this.c.offsetLeft - 210 - 14,
+        y: event.clientY + document.documentElement.scrollTop + document.body.scrollTop - this.c.offsetTop - 95 - 28
+      }
+      console.log(mouse, event.clientX, this.c.offsetTop)
+      // this.ctx.drawImage(img, 100, 20, 28, 28)
+      this.ctx.drawImage(img, mouse.x, mouse.y, 28, 28)
+    },
+    mousedown(e) {
+      const startDot = {
+        x: e.clientX + document.body.scrollLeft - this.c.offsetLeft,
+        y: e.clientY + document.body.scrollHeight - this.c.offsetTop
+      }
+
+      if (this.isAdd) {
+        this.drawIcon(startDot.x, startDot.y)
+      }
+    },
+    drawIcon(x, y) {
+      const img = document.getElementById('icon')
+      this.ctx.drawImage(img, x, y, 18, 18)
     },
     drawLine() {
       this.ctx.beginPath() // 开始路径绘制
@@ -44,9 +72,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#myCanvas{
-  width: 100%;
-  height: 100%;
-  border: 1px solid #000000;
-}
+  @import "~@/styles/variables.scss";
+  img{
+      width: 28px;
+    }
+  .canvas{
+    width: 100%;
+    height: 100%;
+    border: 1px solid #000000;
+  }
 </style>
